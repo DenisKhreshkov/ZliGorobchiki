38ee7ad7fa4e55a217ee5def95eaba2be39356eb
DenisKhreshkov <166539108+DenisKhreshkov@users.noreply.github.com>
2024-04-18 19:51:32 +0200

    Select level manu

--- Assets/Scripts/Denis/ShopController.cs
+++ Assets/Scripts/Denis/ShopController.cs
@@ -4,26 +4,9 @@ using UnityEngine.UI;
 public class ShopController : MonoBehaviour
 {
     [SerializeField] private GameObject ShopPanel;
-    [SerializeField] private Text money;
 
-    [SerializeField] private int totalMoney = 999;
-
-    void Update()
+    public void Back()
     {
-        
-    }
-
-    public void UpdateCoinText()
-    {
-        money.text = totalMoney.ToString();
-    }
-
-    public void BuyTurel1()
-    {
-        if(totalMoney >= 5)
-        {
-            totalMoney -= 5;
-            UpdateCoinText();
-        }
+        ShopPanel.SetActive(false);
     }
 }
---
+++ Assets/Scripts/Denis/ShowText.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class ShowText : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private GameObject showText;
+
+    void Start()
+    {
+        showText.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        showText.SetActive(true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        showText.SetActive(false);
+    }
+
+    public void LVL1()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void LVL2()
+    {
+        SceneManager.LoadScene("");
+    }
+
+    public void LVL3()
+    {
+        SceneManager.LoadScene("");
+    }
+}
