7724cd51baccb78f1339f27f47128ddc28982a6a
Olavr <85408303+TheOlavr@users.noreply.github.com>
2024-04-27 21:57:50 +0300

    bariers script

---
+++ Assets/Scripts/Ostap/Barier Work.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.AI;
+
+public class BarierWork : MonoBehaviour
+{
+    private Slider healthSlider;
+    private ParticleSystem particleSystem;
+    private AudioSource audioSource;
+    private MeshRenderer mainMeshRenderer;
+    private MeshRenderer[] meshRenderers;
+    private NavMeshObstacle navMeshObstacle;
+    [SerializeField] private float liveTime = 10f;
+
+    [SerializeField] Image fill;
+
+    private void Awake()
+    {
+        healthSlider = GetComponentInChildren<Slider>();
+        particleSystem = GetComponentInChildren<ParticleSystem>();
+        audioSource = GetComponent<AudioSource>();
+        meshRenderers = GetComponentsInChildren<MeshRenderer>();
+        mainMeshRenderer = GetComponent<MeshRenderer>();
+        navMeshObstacle = GetComponent<NavMeshObstacle>();
+        healthSlider.maxValue = liveTime;
+        audioSource.volume = PlayerPrefs.GetFloat("Volume");
+        StartCoroutine(BarierLive());
+    }
+
+    private IEnumerator BarierLive()
+    {
+        yield return Time.deltaTime;
+        while (liveTime > 0)
+        {
+            liveTime -= Time.deltaTime;
+            healthSlider.value = liveTime;
+            fill.color = new Color(1-(liveTime / healthSlider.maxValue), liveTime / healthSlider.maxValue, 0);
+            yield return null;
+        }
+        particleSystem.Play();
+        audioSource.Play();
+        if (mainMeshRenderer != null)
+        {
+            mainMeshRenderer.enabled = false;
+        } 
+        else
+        {
+            foreach (MeshRenderer meshRenderer in meshRenderers)
+            meshRenderer.enabled = false;
+        }
+        navMeshObstacle.enabled = false;
+        Destroy(healthSlider.gameObject);
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
+    }
+}
